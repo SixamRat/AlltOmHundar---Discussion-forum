530f172aced0b2d74bd94ae17aa52fc688ae2ac9
SixamRat <saga.martineau@gmail.com>
2025-10-07 11:30:51 +0200

    Updated with missing repos.

---
+++ AlltOmHundar.Core/Interfaces/Repositories/IGroupMemberRepository.cs
@@ -0,0 +1,10 @@
+﻿using AlltOmHundar.Core.Models;
+using System.Threading.Tasks;
+
+namespace AlltOmHundar.Core.Interfaces.Repositories
+{
+    public interface IGroupMemberRepository : IRepository<GroupMember>
+    {
+        Task<GroupMember?> GetMembershipAsync(int groupId, int userId);
+    }
+}
---
+++ AlltOmHundar.Infrastructure/Repositories/GroupMemberRepository.cs
@@ -0,0 +1,20 @@
+﻿using AlltOmHundar.Core.Interfaces.Repositories;
+using AlltOmHundar.Core.Models;
+using AlltOmHundar.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AlltOmHundar.Infrastructure.Repositories
+{
+    public class GroupMemberRepository : Repository<GroupMember>, IGroupMemberRepository
+    {
+        public GroupMemberRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<GroupMember?> GetMembershipAsync(int groupId, int userId)
+        {
+            return await _dbSet.FirstOrDefaultAsync(gm => gm.GroupId == groupId && gm.UserId == userId);
+        }
+    }
+}
