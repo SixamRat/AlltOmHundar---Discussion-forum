314a77190d280e87c9fa71289de4d8b4cbeaf6fd
SixamRat <saga.martineau@gmail.com>
2025-10-08 12:40:06 +0200

    Migrating to Azure

--- AlltOmHundar.Infrastructure/Migrations/20251008103802_UpdateForAzure.Designer.cs
+++ AlltOmHundar.Infrastructure/Migrations/20251008103802_UpdateForAzure.Designer.cs
@@ -4,6 +4,7 @@ using AlltOmHundar.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlltOmHundar.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251008103802_UpdateForAzure")]
+    partial class UpdateForAzure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AlltOmHundar.Infrastructure/Migrations/20251008103802_UpdateForAzure.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlltOmHundar.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateForAzure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "Allmänt hundprat");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "Allmänt hunprat");
+        }
+    }
+}
